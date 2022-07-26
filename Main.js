2f6b37630dcc59c20e70a943fe29aca1635dde07
jude <judeallanhill@icloud.com>
2022-07-25 22:30:22 -0700

    Using Pritier to Make everything nicer

--- Main.js
+++ Main.js
@@ -1,91 +1,90 @@
-import {draw_player} from "./Player.js"
-import {check_player} from "./Player.js"
-import {draw_obstical} from "./Obstical.js"
-import {CheckObstical} from "./Obstical.js"
-import {CheckKeyboard} from "./Keyboard.js"
-import {Setup_Keyboard} from "./Keyboard.js"
+import { draw_player } from "./Player.js";
+import { check_player } from "./Player.js";
+import { draw_obstical } from "./Obstical.js";
+import { CheckObstical } from "./Obstical.js";
+import { CheckKeyboard } from "./Keyboard.js";
+import { Setup_Keyboard } from "./Keyboard.js";
 let bird = {
-    x:20,
-    y:20,
-    vx:0,
-    vy:0.005,
-    ax:0,
-    ay:0.02,
-    alive:true,
-    jump_ready: false,
-    keystate:{
-        'space':false,
-    }
-}
+  x: 20,
+  y: 20,
+  vx: 0,
+  vy: 0.005,
+  ax: 0,
+  ay: 0.02,
+  alive: true,
+  jump_ready: false,
+  keystate: {
+    space: false,
+  },
+};
 let obstical = {
-    x:500,
-    y:0
-}
-let PlayerScore = 0
-let ctx
-let scroll = true
+  x: 500,
+  y: 0,
+};
+let PlayerScore = 0;
+let ctx;
+let scroll = true;
 function Generate_Randome_Num() {
-    obstical.y = (Math.floor(Math.random() * 480))
+  obstical.y = Math.floor(Math.random() * 480);
 }
 function update_physics() {
-    bird.vy = bird.vy + bird.ay
-    bird.y = bird.y + bird.vy
+  bird.vy = bird.vy + bird.ay;
+  bird.y = bird.y + bird.vy;
 }
 function draw_background() {
-    ctx.fillStyle = 'white'
-    ctx.fillRect(0,0,500,500)
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 500, 500);
 }
 
 function scrollX() {
-    if (scroll === true) {
-        obstical.x = obstical.x-1
-        if(bird.x == obstical.x) {
-            console.log("success")
-            PlayerScore += 1
-        }
+  if (scroll === true) {
+    obstical.x = obstical.x - 1;
+    if (bird.x == obstical.x) {
+      console.log("success");
+      PlayerScore += 1;
     }
+  }
 }
 function draw_overlay() {
-    if (bird.alive === false) {
-        ctx.fillStyle = 'red'
-        ctx.font = '24pt sans-serif'
-        ctx.fillText("You Died", 200,50)
-        ctx.fillText("Click To Restart", 150,100)
-        scroll = false
-        obstical.x = 400
-        bird.vx = 0
-        bird.vy = 0
-    }
+  if (bird.alive === false) {
+    ctx.fillStyle = "red";
+    ctx.font = "24pt sans-serif";
+    ctx.fillText("You Died", 200, 50);
+    ctx.fillText("Click To Restart", 150, 100);
+    scroll = false;
+    obstical.x = 400;
+    bird.vx = 0;
+    bird.vy = 0;
+  }
 }
-const $ = (sel) => document.querySelector(sel)
+const $ = (sel) => document.querySelector(sel);
 function score() {
-    $("#Score").innerText = "Score:"+ PlayerScore
+  $("#Score").innerText = "Score:" + PlayerScore;
 }
-function CheckTopWall(){
-    if (bird.y <= 0) {
-        bird.vy = 1
-    }
+function CheckTopWall() {
+  if (bird.y <= 0) {
+    bird.vy = 1;
+  }
 }
 function drawframe() {
-    scrollX()
-    CheckKeyboard(ctx,bird)
-    update_physics()
-    draw_background()
-    draw_player(ctx, bird)
-    check_player(ctx, bird)
-    draw_obstical(ctx, obstical)
-    draw_overlay()
-    CheckObstical(ctx, obstical, bird)
-    CheckTopWall()
-    score()
-    window.requestAnimationFrame(drawframe)
+  scrollX();
+  CheckKeyboard(ctx, bird);
+  update_physics();
+  draw_background();
+  draw_player(ctx, bird);
+  check_player(ctx, bird);
+  draw_obstical(ctx, obstical);
+  draw_overlay();
+  CheckObstical(ctx, obstical, bird);
+  CheckTopWall();
+  score();
+  window.requestAnimationFrame(drawframe);
 }
 function StartGame() {
-    const canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    Generate_Randome_Num()
-    drawframe()
-    Setup_Keyboard(ctx,bird)
+  const canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  Generate_Randome_Num();
+  drawframe();
+  Setup_Keyboard(ctx, bird);
 }
-StartGame()
-
+StartGame();
--- Player.js
+++ Player.js
@@ -1,11 +1,11 @@
 export function draw_player(ctx, bird) {
-    ctx.fillStyle = '#fce056';
-    ctx.fillRect(bird.x, bird.y, 25, 25);
+  ctx.fillStyle = "#fce056";
+  ctx.fillRect(bird.x, bird.y, 25, 25);
 }
 export function check_player(ctx, bird) {
-    if (bird.y >= 475) {
-       bird.vy = 0
-       //bird.ay = 0 
-       bird.y = 0
-   }
+  if (bird.y >= 475) {
+    bird.vy = 0;
+    //bird.ay = 0
+    bird.y = 0;
+  }
 }
